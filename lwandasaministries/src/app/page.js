bc5c06e07a9d2a2fa95e8c02c1a31785d859f105
isaacmatovu <matovuisaac646@gmail.com>
2025-04-11 14:30:34 +0300

    adjusted components layout on mobile devices

--- lwandasaministries/src/app/page.js
+++ lwandasaministries/src/app/page.js
@@ -2,7 +2,6 @@ import EventCards from "@/components/HomePage/EventsCard/EventsCard";
 import Hero from "@/components/HomePage/Hero/Hero";
 import InfoBox from "@/components/HomePage/InfoBox/InfoBox";
 import MinistryCards from "@/components/HomePage/MinistryCard/MinistryCard";
-import Newsletter from "@/components/HomePage/Newsletter/Newsletter";
 import SermonCards from "@/components/HomePage/SermonCard/SermonCard";
 
 export default function Home() {
